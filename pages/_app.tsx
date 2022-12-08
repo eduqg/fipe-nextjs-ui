81ffc993bbcd2893bd634d0ba5f991a15d8315f9
eduqg <eduardoqgomes@gmail.com>
2022-12-08 15:34:50 -0300

    Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
 
-export default function App ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
